3f181bb29256f6ab439167621cb808580e961547
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 13:43:07 +0100

    Controller changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'username' => $data['username'],
+            'dob' => $data['dob']
         ]);
     }
 }
