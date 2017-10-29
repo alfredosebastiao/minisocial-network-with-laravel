4f2a4b2d2e78b880c4fe9fa36562df2d33815fa9
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 14:36:43 +0100

    img src adde

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-body text-center">
-                    pp
-                </div>
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-body text-center">
+                <img src="http://data.whicdn.com/images/64881973/large.jpg">
             </div>
         </div>
     </div>
+   
 @endsection
