52fd3fc4429979569460a924c031debe7b8b7ffa
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 20:19:14 +0100

    Edicao do blate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 10/29/2017
- * Time: 8:06 PM
- */
+@extends('layouts.app')
+@section('content')
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span>Alfredo Sebastiao</span>
+                {{--<span>{{ $post->created_at->diffForHumans }}</span>--}}
+            </div>
+            <div class="panel-body text-center">
+
+            </div>
+            <div class="panel-footer clearfix">
+                <i class="fa fa-heart pull-right"></i>
+            </div>
+        </div>
+    </div>
+
+@endsection
