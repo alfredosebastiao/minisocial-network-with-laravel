a944f01b00f016b46ec805198bf36b0e015e4554
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 16:07:04 +0100

    fields added in post migration

--- database/migrations/2017_10_29_150508_create_posts_table.php
+++ database/migrations/2017_10_29_150508_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->text('content');
             $table->timestamps();
         });
     }
