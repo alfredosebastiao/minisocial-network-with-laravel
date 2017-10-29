708d74349cc63418d31de751891ddac6e8090467
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 14:06:21 +0100

    Route added and profile method added to profilecontroller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function profile($username){
+        $user = User::whereUsername($username)->first();
+
+        return $user;
+    }
 }
