2504fca26f512d830d7757a3afb4502cbac3182a
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 16:52:30 +0100

    View extends ee section etc

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 10/29/2017
- * Time: 4:50 PM
- */
+@extends('layouts.app')
+@section('content')
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading text-center">
+                <h1>Create post</h1>
+            </div>
+            <div class="panel-body text-center">
+
+            </div>
+        </div>
+    </div>
+@endsection
