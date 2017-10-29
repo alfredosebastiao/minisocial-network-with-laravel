aaec76756d8bf81b4837dab0c34c39bd3f573b65
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 22:48:00 +0100

    view show costumizada

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 10/29/2017
- * Time: 10:37 PM
- */
+@extends('layouts.app')
+@section('content')
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span>Alfredo Sebastiao</span>
+                <span class="pull-right">{{ $post->created_at->diffForHumans() }}</span>
+            </div>
+            <div class="panel-body">
+                {{ $post->shortContent }}
+            </div>
+            <div class="panel-footer clearfix" style="background-color: white">
+                <a href="/posts" class="pull-right">Vorltar</a>
+            </div>
+        </div>
+    </div>
+@endsection
