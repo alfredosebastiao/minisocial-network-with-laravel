903264e77cf971e56ad7f5ad5fe460dc8d19c1e3
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 14:50:59 +0100

    Some style to the profile-img

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
+<style>
+    .profile-img{
+        max-width: 150px;
+        border: 5px solid #fff;
+        border-radius: 100%;
+        box-shadow: 0 2px 2px rgba(0,0,0,0.3);
+    }
+</style>
 @section('content')
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-body text-center">
-                <img src="http://data.whicdn.com/images/64881973/large.jpg">
+                <img class="profile-img" src="https://www.neolutionesport.com/wp-content/uploads/2017/03/default-avatar-knives-ninja.png">
                 <h1>{{ $user->username }}</h1>
                 <h5>{{ $user->email }}</h5>
                 <h5>{{ $user->dob->format('l j F Y') }} ( {{ $user->dob->age }} years old)</h5>
+                <button class="btn btn-success">Follow</button>
             </div>
         </div>
     </div>
