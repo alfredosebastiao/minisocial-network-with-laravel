b4a93ceacfe0fc955b7a4b6b0d8ec158fc9c2bf2
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 16:23:39 +0100

    migration to add live fields

--- database/migrations/2017_10_29_152241_add_live_fields_to_the_posts_table.php
+++ database/migrations/2017_10_29_152241_add_live_fields_to_the_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddLiveFieldsToThePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id');
-            $table->text('contentphp');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -28,6 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
