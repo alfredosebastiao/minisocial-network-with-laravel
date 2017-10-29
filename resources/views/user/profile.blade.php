2fcf1648f7683294554984bdc81c1675f7260a2b
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-29 14:31:19 +0100

    painel added

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    BLA
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-body text-center">
+                    pp
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
