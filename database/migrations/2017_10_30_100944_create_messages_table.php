c701fddd21ae6db7fa292bc7973c510bed5ebeb0
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-30 12:24:34 +0200

    create Message Migration Created

--- database/migrations/2017_10_30_100944_create_messages_table.php
+++ database/migrations/2017_10_30_100944_create_messages_table.php
@@ -14,8 +14,22 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id',true);
+            $table->text('mensagem');
+            $table->integer('emissor_id');
+            $table->integer('receptor_id');
             $table->timestamps();
+            $table->foreign('emissor_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+
+            $table->foreign('receptor_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
         });
     }
 
