77997eca51e681d687a604cb1ee043daa43374e3
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-31 18:08:59 +0200

    Migration to create visto field to messge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,8 @@
                                     <li>
                                         <a href="/chats/{{ \Illuminate\Support\Facades\Auth::user()->id }}">
                                             Chats
+                                            <span class="badge badge-default">New</span>
+
                                         </a>
                                     </li>
 
