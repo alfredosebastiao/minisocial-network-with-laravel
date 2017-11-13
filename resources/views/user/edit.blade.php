1c20cca73dfa90561027627d80a6e80cb16a4130
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-11-13 20:30:43 +0200

    Preenchendo campus para edicao

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $user->name }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -29,7 +29,7 @@
                             <label for="username" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                                <input id="username" type="text" class="form-control" name="username" value="{{ $user->username }}" required autofocus>
 
                                 @if ($errors->has('username'))
                                     <span class="help-block">
@@ -43,7 +43,7 @@
                             <label for="dob" class="col-md-4 control-label">Date of Bith</label>
 
                             <div class="col-md-6">
-                                <input id="dob" type="date" class="form-control" name="dob" value="{{ old('dob') }}" required autofocus>
+                                <input id="dob" type="date" class="form-control" name="dob" value="{{ $user->dob->format('Y-m-d') }}" required autofocus>
 
                                 @if ($errors->has('dob'))
                                     <span class="help-block">
@@ -57,7 +57,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}" required >
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -67,32 +67,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Update
                                 </button>
                             </div>
                         </div>
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -15,7 +15,7 @@
                 <h1>{{ $user->username }}</h1>
                 <h5>{{ $user->email }}</h5>
                 <h5>{{ $user->dob->format('l j F Y') }} ( {{ $user->dob->age }} years old)</h5>
-                <a href="profile/{{ $user->username }}/edit" class="btn btn-success">Edit</a>
+                <a href="{{ $user->username }}/edit" class="btn btn-success">Edit</a>
             </div>
         </div>
     </div>
